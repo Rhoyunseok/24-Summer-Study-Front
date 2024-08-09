12d00a2df301dba67a1dd9852bbb4737bcd97f48
Rhoyunseok <74577789+Rhoyunseok@users.noreply.github.com>
2024-08-09 16:32:26 +0900

    chore: Update gitignore to ignore build files and additional node_modules

--- 03-JSX/next-jsx-app/pages/_app.tsx
+++ 03-JSX/next-jsx-app/pages/_app.tsx
@@ -1,6 +1,10 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+//전역 css파일 참조하여 리엑트앱 전체 컴포넌트에서 스타일 사용가능
+import '@/styles/globals.css';
+
+//App 최상위 컴포넌트에 전달되는 파라메터의 타입참조하기
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
+  //Componenet파라메터는 라우팅 주소에 따라 변경되는 컨텐츠 페이지 컴포넌트를 의미합니다.
   return <Component {...pageProps} />;
 }
