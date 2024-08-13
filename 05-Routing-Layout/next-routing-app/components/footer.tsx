0f5e0b1ca06529362ef98c417ab046f3fdab97db
Rhoyunseok <74577789+Rhoyunseok@users.noreply.github.com>
2024-08-13 13:34:30 +0900

    chore: Update footer and header components

--- 05-Routing-Layout/next-routing-app/components/footer.tsx
+++ 05-Routing-Layout/next-routing-app/components/footer.tsx
@@ -1,12 +1,16 @@
 const Footer = () => {
   return (
     <div
+      className="text-center"
       style={{
         width: "100%",
+        backgroundColor: "black",
         height: "50px",
-        backgroundColor: "#333",
+        color: "white",
       }}
-    ></div>
+    >
+      Footer-CopyRight...
+    </div>
   );
 };
 
